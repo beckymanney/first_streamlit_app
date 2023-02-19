3dab1e8341a3016a289e8ee6620bdd0800be9ebb
Becky Manney <83999113+beckymanney@users.noreply.github.com>
2023-02-19 13:05:27 -0600

    Update streamlit_app.py
    deleted one data frame.  I created two

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,3 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
-
-streamlit.dataframe(my_fruit_list)
