5f66609872c07f4833edd6cc0aee7f34409b4101
Becky Manney <83999113+beckymanney@users.noreply.github.com>
2023-02-20 23:38:54 -0600

    Update streamlit_app.py
    Deleted raw json data
    Added Kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,7 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just writes the data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # take the json version of the response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
