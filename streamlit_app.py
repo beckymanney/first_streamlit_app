5450cf4a1a48bd10545bdf991f4064e42d4a9095
Becky Manney <83999113+beckymanney@users.noreply.github.com>
2023-02-24 23:08:53 -0600

    Update streamlit_app.py
    Add all rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,9 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 streamlit.title("My Mom's New Healthy Diner")
 
