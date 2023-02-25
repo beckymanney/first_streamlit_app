ff0105e1e01306303b6932580b8ee22fa644d5ce
Becky Manney <83999113+beckymanney@users.noreply.github.com>
2023-02-24 23:25:31 -0600

    Update streamlit_app.py
    Added Organization
    Added Error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
+
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
