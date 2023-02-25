79878baa73193bc9ea2133af61d07c236c2921af
Becky Manney <83999113+beckymanney@users.noreply.github.com>
2023-02-25 00:54:57 -0600

    Update streamlit_app.py
    move close button

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,8 +69,8 @@ def insert_row_snowflake(new_fruit):
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-     my_cnx.close()
      back_from_function = insert_row_snowflake(add_my_fruit)
+     my_cnx.close()
      streamlit.text(back_from_function)
 
 # don't run anything past here while we troubleshoot
