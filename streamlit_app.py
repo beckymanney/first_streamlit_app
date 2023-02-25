80d929d51123488f4d9b96a139e0487e2505d31e
Becky Manney <83999113+beckymanney@users.noreply.github.com>
2023-02-24 23:13:40 -0600

    Update streamlit_app.py
    Add second entry box for additional fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-
+#Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
+streamlit.write('Thanks for adding jackfruit')
