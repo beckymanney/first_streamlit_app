68a0aa084f1c7e254ccefa89e9aba75f04b5d3ad
Becky Manney <83999113+beckymanney@users.noreply.github.com>
2023-02-24 23:49:57 -0600

    Update streamlit_app.py
     What Happens When We Type a Fruit Name in the TEXT_INPUT and Hit the Enter Key?

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #create the repeatable code block (called a function)
-def get_fruityvice_data(this_fruit_choice):
+def get_fruityvice_data(kiwi):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
@@ -40,8 +40,8 @@ try:
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
-      back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_function)
+      back_from_function = fruityvice_normalized
+      streamlit.dataframe back_from_function
       
 except URLError as e:
   streamlit.error()
