952da3367162e7cc4fe5276970dc4dfe8a130780
Becky Manney <83999113+beckymanney@users.noreply.github.com>
2023-02-24 23:27:44 -0600

    Update streamlit_app.py
    moved code

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
 streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
@@ -51,6 +41,16 @@ streamlit.dataframe(fruityvice_normalized)
 #Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding jackfruit')
+
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
 #This will not work correctly, but just go with it for now
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
